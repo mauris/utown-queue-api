d69d8f9a4a9de30fe50d3089f2663cda7e9d1b57
Sam-Mauris Yong <mauris@hotmail.sg>
2016-10-20 23:14:39 +0800

    added auth check middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,37 @@
+let models  = require('utown-queue-db');
+let Promise = require('bluebird');
+let bcrypt  = Promise.promisifyAll(require('bcryptjs'));
+let jwt     = Promise.promisifyAll(require('jsonwebtoken'));
+
+module.exports = (req, res, next) => {
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  if (!token) {
+    return res
+      .status(403)
+      .json({ "error": "Authentication Failed." });
+  }
+  jwt.verifyAsync(token, process.env.API_AUTH_SECRET)
+    .then((decoded) => {
+      return models.Event
+        .find({
+          where: {
+            eventCode: decoded.code,
+            secret: decoded.secret
+          }
+        });
+    })
+    .then((event) => {
+      if (!event) {
+        throw new Error();
+      }
+      req.event = event;
+      next();
+    })
+    .catch((err) => {
+      res
+        .status(403)
+        .json({
+          "error": "Authentication failed."
+        });
+    });
+};
