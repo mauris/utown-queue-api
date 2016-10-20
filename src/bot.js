ea35885591124bfcba64a41bcb35726e50e29ea9
Sam-Mauris Yong <mauris@hotmail.sg>
2016-10-20 23:14:16 +0800

    added bot instance

---
+++ src/bot.js
@@ -0,0 +1,6 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.TELEGRAM_TOKEN || '';
+
+// no polling for the bot because we simply just want to send out messages
+
+module.exports = new TelegramBot(token, options);
