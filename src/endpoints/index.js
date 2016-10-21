0d57a624e5c8263564a642c4292f35a1ac159749
Sam-Mauris Yong <mauris@hotmail.sg>
2016-10-21 17:17:17 +0800

    added support for CORS

--- src/endpoints/index.js
+++ src/endpoints/index.js
@@ -2,6 +2,19 @@ const path = require("path");
 
 module.exports = (app) => {
   let API_PREFIX = "/v1";
+  if (!process.env.CORS_CLIENT_DOMAIN) {
+    console.log('[WARN] The CORS_CLIENT_DOMAIN for Cross Origin Control is not set.')
+  }
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', process.env.CORS_CLIENT_DOMAIN || '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With');
+    if ('OPTIONS' === req.method) {
+      return res.status(200).send();
+    }
+    next();
+  });
 
   app.use(API_PREFIX + '/auth', require('./auth'));
   app.use(API_PREFIX + '/groups', require('./groups'));
