abbd8d4e9ce3a793be410ad5dc127cf725d953a3
Sam-Mauris Yong <mauris@hotmail.sg>
2016-10-23 01:05:53 +0800

    added cancel tickets functions

--- src/endpoints/tickets.js
+++ src/endpoints/tickets.js
@@ -5,6 +5,45 @@ const authChecker = require('../middleware/auth');
 
 module.exports = router;
 
+let cancelTicket = (ticket, transaction) => {
+  if (!transaction) {
+    return models.sequelize
+      .transaction((t) => {
+        return cancelTicket(ticket, user, t);
+      });
+  }
+  return Promise.all([
+    ticket.update(
+      { isActive: false },
+      { transaction: transaction }
+    ),
+    models.User.update(
+      { isInQueue: false },
+      { where: { userId: ticket.userId }, transaction: transaction }
+    )
+  ]);
+};
+
+let updateGroupAndCancelTicket = (group, ticket, transaction) => {
+  if (!transaction) {
+    return models.sequelize
+      .transaction((t) => {
+        return updateGroupAndCancelTicket(group, ticket, t);
+      });
+  }
+
+  // if there is only one ticket left and the last ticket cancels, then we can say good bye to the group.
+  if (group.totalNoOfPeople === ticket.noOfPeople) {
+    return cancelTicket(ticket, transaction)
+      .then(group.destroy({transaction: transaction}));
+  }
+
+  return Promise.all([
+    cancelTicket(ticket, transaction),
+    group.decrement("totalNoOfPeople", {by: ticket.noOfPeople})
+  ])
+};
+
 router.get('/', authChecker, (req, res, next) => {
   req.event.getTickets({ where: { datetimeStart: null } })
     .then((tickets) => {
