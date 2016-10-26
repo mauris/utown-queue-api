2f8660e8739b7f0cfbba4a653924df0a382065ac
Sam-Mauris Yong <mauris@hotmail.sg>
2016-10-26 13:13:19 +0800

    implemented event queue remiinder

---
+++ src/daemon/eventQueueReminder.js
@@ -0,0 +1,36 @@
+const models = require('utown-queue-db');
+const Promise = require('bluebird');
+const bot = require('../bot');
+
+console.log('Daemon worker #' + process.pid + ' started.');
+
+let $controller = () => {
+  return models.Event
+    .findAll({})
+    .then((events) => {
+      return Promise.map(events, (event) => {
+        return event.getGroups({ where: { datetimeStart: null }, include: [{ model: models.Ticket, as: 'tickets' }] })
+          .then((groups) => {
+            var promises = [];
+            var count = 0;
+            groups.forEach((group) => {
+              if (!group.tickets) {
+                return;
+              }
+              group.tickets.forEach((ticket) => {
+                if (count < 3) {
+                  promises.push(bot.sendMessage(ticket.userId, "Your turn in the queue at " + event.eventName +" should be coming up real soon. The usher will message you to make your way to the entrance. The current estimated waiting time is " + Math.ceil(event.averageWaitingTime / 60) + " mins."));
+                  return;
+                }
+                promises.push(bot.sendMessage(ticket.userId, "There are about " + count + " groups before yours in the queue at " + event.eventName +". The current estimated waiting time is " + Math.ceil(event.averageWaitingTime / 60) + " mins."));
+              })
+              ++count;
+            });
+            return Promise.all(promises);
+          });
+      });
+    });
+};
+
+$controller();
+setInterval($controller, 600000);
--- src/daemon/index.js
+++ src/daemon/index.js
@@ -3,3 +3,4 @@ const child_process = require('child_process');
 child_process.fork('src/daemon/waitingTimeUpdater.js');
 child_process.fork('src/daemon/groupMatcher.js');
 child_process.fork('src/daemon/eventCodeChanger.js');
+child_process.fork('src/daemon/eventQueueReminder.js');
