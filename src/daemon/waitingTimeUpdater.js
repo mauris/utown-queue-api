42db8fc10c2c1dd86a91ec88843ec62b9484c3be
Sam-Mauris Yong <mauris@hotmail.sg>
2016-10-27 22:46:34 +0800

    fix name case sensitivity problem with update query

--- src/daemon/waitingTimeUpdater.js
+++ src/daemon/waitingTimeUpdater.js
@@ -3,7 +3,7 @@ const models = require('utown-queue-db');
 console.log('Daemon worker #' + process.pid + ' started.');
 
 let $controller = () => {
-  models.sequelize.query('UPDATE events RIGHT JOIN (SELECT AVG(TIMESTAMPDIFF(SECOND, datetimeFormed, datetimeStart)) AS AverageWaitingTime, eventId FROM groups WHERE datetimeStart IS NOT NULL GROUP BY eventId) groupwt ON groupwt.eventId = events.eventId RIGHT JOIN (SELECT AVG(TIMESTAMPDIFF(SECOND, datetimeRequested, datetimeStart)) AS AverageFormingTime, eventId FROM tickets WHERE datetimeStart IS NOT NULL GROUP BY eventId) AS ticketwt ON ticketwt.eventId = events.eventId SET events.AverageWaitingTime = groupwt.AverageWaitingTime + ticketwt.AverageFormingTime', { type: models.sequelize.QueryTypes.UPDATE });
+  models.sequelize.query('UPDATE Events RIGHT JOIN (SELECT AVG(TIMESTAMPDIFF(SECOND, datetimeFormed, datetimeStart)) AS AverageWaitingTime, eventId FROM Groups WHERE datetimeStart IS NOT NULL GROUP BY eventId) groupwt ON groupwt.eventId = events.eventId RIGHT JOIN (SELECT AVG(TIMESTAMPDIFF(SECOND, datetimeRequested, datetimeStart)) AS AverageFormingTime, eventId FROM Tickets WHERE datetimeStart IS NOT NULL GROUP BY eventId) AS ticketwt ON ticketwt.eventId = events.eventId SET Events.AverageWaitingTime = groupwt.AverageWaitingTime + ticketwt.AverageFormingTime', { type: models.sequelize.QueryTypes.UPDATE });
 };
 
 $controller();
