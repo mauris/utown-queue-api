510e208e0704499d5587116471d285e397b794fd
Sam-Mauris Yong <mauris@hotmail.sg>
2016-10-28 20:23:57 +0800

    updated waitingTimeUpdater to be more accurate

--- src/daemon/waitingTimeUpdater.js
+++ src/daemon/waitingTimeUpdater.js
@@ -3,7 +3,7 @@ const models = require('utown-queue-db');
 console.log('Daemon worker #' + process.pid + ' started.');
 
 let $controller = () => {
-  models.sequelize.query('UPDATE Events RIGHT JOIN (SELECT AVG(TIMESTAMPDIFF(SECOND, datetimeFormed, datetimeStart)) AS AverageWaitingTime, eventId FROM Groups WHERE datetimeStart IS NOT NULL GROUP BY eventId) groupwt ON groupwt.eventId = Events.eventId RIGHT JOIN (SELECT AVG(TIMESTAMPDIFF(SECOND, datetimeRequested, datetimeStart)) AS AverageFormingTime, eventId FROM Tickets WHERE datetimeStart IS NOT NULL GROUP BY eventId) AS ticketwt ON ticketwt.eventId = Events.eventId SET Events.AverageWaitingTime = groupwt.AverageWaitingTime + ticketwt.AverageFormingTime', { type: models.sequelize.QueryTypes.UPDATE });
+  models.sequelize.query('UPDATE Events RIGHT JOIN (SELECT AVG(TIMESTAMPDIFF(SECOND, datetimeFormed, datetimeStart)) / COUNT(GroupId) * (SELECT COUNT(*) FROM Groups AS G WHERE G.eventId = Groups.eventId AND datetimeStart IS NULL) AS AverageWaitingTime, eventId FROM Groups WHERE datetimeStart IS NOT NULL GROUP BY eventId) groupwt ON groupwt.eventId = Events.eventId RIGHT JOIN (SELECT AVG(TIMESTAMPDIFF(SECOND, datetimeRequested, datetimeStart)) AS AverageFormingTime, eventId FROM Tickets WHERE datetimeStart IS NOT NULL GROUP BY eventId) AS ticketwt ON ticketwt.eventId = Events.eventId SET Events.AverageWaitingTime = groupwt.AverageWaitingTime + ticketwt.AverageFormingTime', { type: models.sequelize.QueryTypes.UPDATE });
 };
 
 $controller();
